c248454cfc7f7275e6a03be10cbf245c2dc6b0f1
Basty <banny444666@gmail.com>
2024-09-09 16:46:26 +0300

    1-3 tasks

--- L1/tasks/task_03/3.go
+++ L1/tasks/task_03/3.go
@@ -1,5 +1,40 @@
 package task_03
 
-func Ex3() {
+import (
+	"fmt"
+	"sync"
+)
 
+func Check() {
+
+	a := [5]int{2, 4, 6, 8, 10}
+
+	resultChan := make(chan int, len(a))
+
+	var wg sync.WaitGroup
+
+	wg.Add(len(a))
+
+	for _, v := range a {
+		go func(n int) {
+			wg.Done()
+			square(v, resultChan)
+		}(v)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	sum := 0
+	for i := 0; i < len(a); i++ {
+		// Суммируем значения из канала
+		sum += <-resultChan
+	}
+	fmt.Printf(" Итоговое значение: %d", sum)
+}
+
+func square(n int, resultChan chan int) {
+	resultChan <- n * n
 }
